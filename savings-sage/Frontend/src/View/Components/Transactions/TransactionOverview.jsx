3e3c240b12e5135e046ab15bc53eb01581dcebd6
JeanetteMoKa <zsanettkasek@gmail.com>
2024-08-21 15:44:04 +0200

    bugfix frontend conditional reverse

--- savings-sage/Frontend/src/View/Components/Transactions/TransactionOverview.jsx
+++ savings-sage/Frontend/src/View/Components/Transactions/TransactionOverview.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function TransactionOverview({setIsNewBeingAdded, allAccounts}) {
     return (<>
-            {allAccounts.length > 0 ? (<p>You do not have an account. You need to create an account before you can add transactions.</p>):
+            {allAccounts.length === 0 ? (<p>You do not have an account. You need to create an account before you can add transactions.</p>):
                 (<button className="mainButton" onClick={() => setIsNewBeingAdded(true)}>Add new transaction</button>)}
     </>);
 }
